fce04bb5c6308088d95773477e9d89c4d2b9609d
tyh4n <tianyih4@illinois.edu>
2025-05-28 18:47:40 -0500

    adding testing model for rolling contact

--- sim_test.m
+++ sim_test.m
@@ -1,14 +1,17 @@
-% Start simulation in normal mode
+%% Linear Evaluation
 accel_time_arr = linspace(0.2, 5, 5);
 
 for i = 1:length(accel_time_arr)
-    fail_flag = 0;
-
     accel_time = accel_time_arr(i);
-    sim_out = sim('ballbot');
-
-    if fail_flag ~= 0
-        disp("System failed")
+    % Run simulation
+    sim_out = sim('ballbot', 'StopTime', '10');  
+    
+    tout = sim_out.get('tout');
+    % Check if simulation ran to completion
+    if tout(end) < 10
+        disp('Simulation was interrupted!');
+    else
+        disp('Simulation completed normally.');
     end
 
     video_file_name = "test" + num2str(i);
